08d6764a21e014afb4ad170e5d9e0ed225b02702
Esteban Cespedes <ces.esteban@gmail.com>
2020-11-15 20:46:59 -0300

    Updates

---
+++ precache-manifest.a2a6476a8ac25e07a508bbd720eabd98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cc91c60d648201f512f8e2bc5f3af6",
+    "url": "/my-cripto-app/index.html"
+  },
+  {
+    "revision": "a9214fca7a026c0c23da",
+    "url": "/my-cripto-app/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "a9214fca7a026c0c23da",
+    "url": "/my-cripto-app/static/js/2.c41d71a4.chunk.js"
+  },
+  {
+    "revision": "3a24589c4bb9995cbc50abf564c6ffde",
+    "url": "/my-cripto-app/static/js/2.c41d71a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a065adf128bbc0751a",
+    "url": "/my-cripto-app/static/js/main.8caffacd.chunk.js"
+  },
+  {
+    "revision": "41f7fb5a7218774d3eb3",
+    "url": "/my-cripto-app/static/js/runtime-main.efb8bbd3.js"
+  }
+]);
